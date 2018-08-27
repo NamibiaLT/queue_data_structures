a56e677626e7666355518a2089046360cf41a9af
NLT <NamibiaLT@users.noreply.github.com>
2018-08-27 16:58:20 -0700

    initial commits

---
+++ lib/queue_circular.rb
@@ -0,0 +1,65 @@
+class MyCircularQueue
+=begin
+    Initialize your data structure here. Set the size of the queue to be k.
+    :type k: Integer
+=end
+    def initialize(k)
+        @k = k
+    end
+
+    arr = Array.new(k)
+
+=begin
+    Insert an element into the circular queue. Return true if the operation is successful.
+    :type value: Integer
+    :rtype: Boolean
+=end
+    def en_queue(value)
+      if arr.nil?
+        arr.shift
+      end
+      arr << value
+      arr.include?(value)
+    end
+
+=begin
+    Delete an element from the circular queue. Return true if the operation is successful.
+    :rtype: Boolean
+=end
+    def de_queue
+      arr.delete(arr.sample(1, random: rng))
+      arr.size < k
+    end
+
+=begin
+    Get the front item from the queue.
+    :rtype: Integer
+=end
+    def front()
+      arr.first # also written as: arr[0] || arr.at(0)
+    end
+
+=begin
+    Get the last item from the queue.
+    :rtype: Integer
+=end
+    def rear()
+      arr.last # also written as: arr[-1]
+    end
+
+=begin
+    Checks whether the circular queue is empty or not.
+    :rtype: Boolean
+=end
+    def is_empty()
+      arr.empty?
+    end
+
+=begin
+    Checks whether the circular queue is full or not.
+    :rtype: Boolean
+=end
+    def is_full()
+      arr.size == k
+    end
+end
---
+++ lib/queue_inout.rb
@@ -0,0 +1,23 @@
+# Practice Queue In and Out structures
+# THe first element added to the queue gets removed first
+# The next element added to the queue gets positioned in last place
+
+class InOutQueue
+  attr_reader :arr
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def dequeue
+    @arr.delete_at(0)
+  end
+
+  def enqueue(element)
+      @arr << element
+  end
+end
+
+queue = InOutQueue.new([1,2])
+
+puts queue.dequeue
+puts queue.enqueue(3)
